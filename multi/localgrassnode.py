b93a4c59c312881d20f216631d523d761bb11be7
pemujaraptor <169087612+pemujaraptor@users.noreply.github.com>
2024-11-01 02:49:19 +0700

    Add files via upload

---
+++ multi/localgrassnode.py
@@ -0,0 +1,98 @@
+import asyncio
+import random
+import ssl
+import json
+import time
+import uuid
+import requests
+import shutil
+from loguru import logger
+from websockets_proxy import Proxy, proxy_connect
+from fake_useragent import UserAgent
+
+user_agent = UserAgent()
+random_user_agent = user_agent.random
+
+
+async def connect_to_wss(socks5_proxy, user_id):
+    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
+    logger.info(device_id)
+    while True:
+        try:
+            await asyncio.sleep(random.randint(1, 10) / 10)
+            custom_headers = {
+                "User-Agent": random_user_agent,
+                "Origin": "chrome-extension://lkbnfiajjmbhnfledhphioinpickokdi"
+            }
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+            uri = "wss://proxy.wynd.network:4650/"
+            server_hostname = "proxy.wynd.network"
+            proxy = Proxy.from_url(socks5_proxy)
+            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
+                                     extra_headers=custom_headers) as websocket:
+                async def send_ping():
+                    while True:
+                        send_message = json.dumps(
+                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
+                        logger.debug(send_message)
+                        await websocket.send(send_message)
+                        await asyncio.sleep(20)
+
+                await asyncio.sleep(1)
+                asyncio.create_task(send_ping())
+
+                while True:
+                    response = await websocket.recv()
+                    message = json.loads(response)
+                    logger.info(message)
+                    if message.get("action") == "AUTH":
+                        auth_response = {
+                            "id": message["id"],
+                            "origin_action": "AUTH",
+                            "result": {
+                                "browser_id": device_id,
+                                "user_id": user_id,
+                                "user_agent": custom_headers['User-Agent'],
+                                "timestamp": int(time.time()),
+                                "device_type": "extension",
+                                "version": "4.26.2",
+                                "extension_id": "lkbnfiajjmbhnfledhphioinpickokdi"
+                            }
+                        }
+                        logger.debug(auth_response)
+                        await websocket.send(json.dumps(auth_response))
+
+                    elif message.get("action") == "PONG":
+                        pong_response = {"id": message["id"], "origin_action": "PONG"}
+                        logger.debug(pong_response)
+                        await websocket.send(json.dumps(pong_response))
+        except Exception as e:
+            logger.error(e)
+            logger.error(socks5_proxy)
+
+
+async def main():
+    # Membaca user_id dari file user_id.txt
+    with open('user_id.txt', 'r') as user_file:
+        user_ids = user_file.read().splitlines()
+    
+    # Menampilkan jumlah akun
+    print(f"Jumlah akun: {len(user_ids)}")
+    
+    with open('local_proxies.txt', 'r') as proxy_file:
+        local_proxies = proxy_file.read().splitlines()
+    
+    # Membuat tugas untuk setiap user_id dengan proxy secara bergantian
+    tasks = []
+    proxy_count = len(local_proxies)
+    for index, user_id in enumerate(user_ids):
+        proxy = local_proxies[index % proxy_count]  # Memilih proxy secara bergantian
+        tasks.append(asyncio.ensure_future(connect_to_wss(proxy, user_id)))
+    
+    await asyncio.gather(*tasks)
+
+if __name__ == '__main__':
+    #letsgo
+    asyncio.run(main())
